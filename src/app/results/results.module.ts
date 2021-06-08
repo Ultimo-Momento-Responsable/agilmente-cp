aff732cf5259c36408ebd391f903e8bc7d3cfd84
julianp950 <julian.p950@gmail.com>
2021-06-08 18:50:21 -0300

    Feat: Implemented reading from JSON in Results page

--- src/app/results/results.module.ts
+++ src/app/results/results.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
-
 import { ResultsPageRoutingModule } from './results-routing.module';
-
 import { ResultsPage } from './results.page';
 
 @NgModule({
--- src/app/results/results.page.ts
+++ src/app/results/results.page.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+export interface Result {
+  id: string;
+  name: string;
+  patient: string;
+  aciertos: string;
+}
+
+export interface RootObject {
+  results: Result[];
+}
 
 @Component({
   selector: 'app-results',
   templateUrl: './results.page.html',
   styleUrls: ['./results.page.scss'],
 })
-export class ResultsPage implements OnInit {
 
-  constructor() { }
+export class ResultsPage implements OnInit {
+  results: Array<any>;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getResults().subscribe(res =>{
+        console.log("Res",res)
+        this.results = res;
+    });
   }
 
+  getResults(){
+    return this.http
+    .get("assets/results.json")
+    .pipe(
+      map((res:any) => {
+        return res.results;
+      })
+    )
+  }
 }
