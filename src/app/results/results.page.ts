fb36d7a277d0d8c4b68ee3cbbd3baa3e4e6519c3
ArmatorX <17768182+ArmatorX@users.noreply.github.com>
2021-06-17 18:27:17 -0300

    Chore: Unused imports

--- src/app/results/results.page.ts
+++ src/app/results/results.page.ts
@@ -1,38 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { ResultsApiService } from './services/results-api.service';
 
 export interface Result {
-  id: number,
-  name: string,
-  patient: string,
-  successes: number,
-  mistakes: number,
-  timeBetweenSuccesses: number[],
-  date: string,
-  totalTime: number,
-  canceled: boolean
+    id: number,
+    name: string,
+    patient: string,
+    successes: number,
+    mistakes: number,
+    timeBetweenSuccesses: number[],
+    date: string,
+    totalTime: number,
+    canceled: boolean
 }
 
 @Component({
-  selector: 'app-results',
-  templateUrl: './results.page.html',
-  styleUrls: ['./results.page.scss'],
+    selector: 'app-results',
+    templateUrl: './results.page.html',
+    styleUrls: ['./results.page.scss'],
 })
 
 export class ResultsPage implements OnInit {
 
-  results: any[];
-  constructor(private http: HttpClient,
-    private resultsApiService: ResultsApiService) { }
+    results: any[];
+    constructor(private resultsApiService: ResultsApiService) { }
 
-  ngOnInit() { }
+    ngOnInit() { }
 
-  ionViewWillEnter() {
-    this.resultsApiService.getResults().subscribe(res =>{
-      this.results = res.content;
-    });
-  }
+    ionViewWillEnter() {
+        this.resultsApiService.getResults().subscribe(res => {
+            this.results = res.content;
+        });
+    }
 
 }
