d62091e1e7ce5da37cea2f7097180cd905783aad
Fenlethmarg <guillermo.gribaudo95@gmail.com>
2021-10-04 20:45:51 -0300

    :sparkles: Feat (Planning): adjust the new param

--- src/app/planning/new-planning/new-planning.page.ts
+++ src/app/planning/new-planning/new-planning.page.ts
@@ -52,7 +52,7 @@ export class NewPlanningPage implements OnInit {
             p.value=1;
           }
           if (p.param.type == 3) {
-            p.value=p.minValue;
+            p.value=(p.minValue + p.maxValue) / 2;
           }
         });
         this.games[i].maxNumberOfSessions = 5;
