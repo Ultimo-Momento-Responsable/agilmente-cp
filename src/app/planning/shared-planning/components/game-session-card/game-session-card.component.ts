3cf3a446f0a7d7fbb7e6de2aac2ce4daf719d613
ArmatorX <17768182+ArmatorX@users.noreply.github.com>
2022-06-01 19:04:43 -0300

    :sparkles: Feat (Plannings): WIP: Add game session card component

---
+++ src/app/planning/shared-planning/components/game-session-card/game-session-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-session-card',
+  templateUrl: './game-session-card.component.html',
+  styleUrls: ['./game-session-card.component.scss'],
+})
+export class GameSessionCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
