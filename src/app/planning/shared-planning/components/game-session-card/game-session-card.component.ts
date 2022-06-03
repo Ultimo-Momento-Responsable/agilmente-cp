9dcde560621cbc3adcc725e5d82c8a2ee84fe431
ArmatorX <17768182+ArmatorX@users.noreply.github.com>
2022-06-03 19:02:56 -0300

    :sparkles: Feat (Plannings): Add params to accordion

--- src/app/planning/shared-planning/components/game-session-card/game-session-card.component.ts
+++ src/app/planning/shared-planning/components/game-session-card/game-session-card.component.ts
@@ -7,6 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameSessionCardComponent implements OnInit {
   tendency: string;
+  params = [
+    {
+      name: "Nivel Maximo",
+      value: 999,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+    },
+    {
+      name: "Nivel",
+      value: 999,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque id egestas dolor. Quisque egestas vehicula turpis, ac maximus ante. Fusce in orci in ligula viverra tempor non porta est. "
+    },
+    {
+      name: "Tu Hermana",
+      value: "Oye, si",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque id egestas dolor."
+    },
+    {
+      name: "Nivel Maximo",
+      value: 999,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque id egestas dolor. Quisque egestas vehicula turpis, ac maximus ante. Fusce in orci in ligula viverra tempor non porta est. Nulla feugiat ex et convallis sagittis."
+    },
+  ];
   constructor() { }
 
   ngOnInit() {
