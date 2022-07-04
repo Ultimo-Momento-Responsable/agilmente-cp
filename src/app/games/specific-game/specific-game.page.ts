cc0d6c417b3f9419fad0b32d990f11ce7fd06759
ArmatorX <17768182+ArmatorX@users.noreply.github.com>
2022-07-04 11:43:18 -0300

    :memo: Refactor (Games): Simplify implementation using constants

--- src/app/games/specific-game/specific-game.page.ts
+++ src/app/games/specific-game/specific-game.page.ts
@@ -1,18 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { GamesApiService } from '../services/games-api.service';
+import { Game, GamesApiService } from '../services/games-api.service';
 import { ActivatedRoute } from '@angular/router';
-import { HttpClient } from "@angular/common/http";
 import { IonSlides} from '@ionic/angular';
-
-export interface Game {
-  id: number;
-  name: string;
-  description: string;
-  paramDescription: string;
-  cognitiveDomain: any[];
-  gameParam: any[];
-  params: any[];
-}
+import { GAMES } from 'src/app/shared/constants/games.constant';
 
 export interface SlideImg {
   img: String;
@@ -35,8 +25,7 @@ export class SpecificGamePage implements OnInit {
 
   constructor(
     private gamesApiService: GamesApiService,
-    private route: ActivatedRoute,
-    private httpClient: HttpClient
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit() { }
@@ -47,9 +36,9 @@ export class SpecificGamePage implements OnInit {
       this.gamesApiService.getGameById(this.id).subscribe((res) => {
         this.specificGame = res;
         this.gameThumb = this.getGameThumb(res);
-        this.getGameDescription(res);
-        this.findActivatable(res);
-        this.findType3(res);
+        this.getGameDescription();
+        this.findActivatable();
+        this.findType3();
         this.getGameScreenshot(res);
       });
       
@@ -57,35 +46,27 @@ export class SpecificGamePage implements OnInit {
   }
   // Obtiene un juego y devuelve el nombre del archivo PNG del ícono
   // @param game Objeto juego
-  getGameThumb(game) {
+  getGameThumb(game: Game) {
     let gameNameFormatted : string = game.name.toLowerCase();
     gameNameFormatted = gameNameFormatted.replace(/\s/g, '_');
     return( "../../../assets/pictures/" + gameNameFormatted + "_icon.png");
   }
 
-  // Obtiene un juego y devuelve la explicación detallada desde el JSON
-  // ubicada en la carpeta assets.
-  // @param game Objeto juego
-  getGameDescription(game) {
-    let gameData: any [];
-    let URL = "../../../assets/games.json";
-    this.httpClient.get(URL).subscribe(data =>{
-        gameData = JSON.parse(JSON.stringify(data));;
-        this.gameDescription = gameData[this.id-1].description;
-    })
+  // Obtiene un juego y devuelve la explicación detallada desde el la
+  // constante de juegos.
+  getGameDescription() {
+      this.gameDescription = GAMES[this.id-1].description;
   }
 
   // Obtiene un juego y guarda en una variable si tiene parámetros del tipo 1
-  // @param game Objeto juego
-  findActivatable(game) {
-    this.hasActivatable = this.specificGame.params.find( result => {
+  findActivatable() {
+    this.hasActivatable = this.specificGame.params.find(result => {
       return result.type === 1;
     })
   }
 
   // Obtiene un juego y guarda en una variable si tiene parámetros del tipo 3
-  // @param game Objeto juego
-  findType3(game) {
+  findType3() {
     this.hasType3 = this.specificGame.params.find( result => {
       return result.type === 3;
     })
@@ -108,7 +89,7 @@ export class SpecificGamePage implements OnInit {
 
   // Obtiene un juego y llena el array de screenshots para el slides
   // @param game Objeto juego
-  getGameScreenshot(game) {
+  getGameScreenshot(game: Game) {
     let gameNameFormatted : string = game.name.toLowerCase();
     gameNameFormatted = gameNameFormatted.replace(/\s/g, '_');
     for (let i = 1; i < 6; i++) {
