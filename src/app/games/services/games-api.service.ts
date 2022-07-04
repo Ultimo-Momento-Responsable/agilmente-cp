36cf022ca2e724650440d5c79752d6954fcc61cc
ArmatorX <17768182+ArmatorX@users.noreply.github.com>
2022-07-04 11:44:47 -0300

    :sparkles: Feat (Games): Add new endpoints to games

--- src/app/games/services/games-api.service.ts
+++ src/app/games/services/games-api.service.ts
@@ -1,7 +1,37 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { CognitiveDomain } from 'src/app/cognitive-domain/service/cognitive-domain-api.service';
 import { environment } from 'src/environments/environment';
+export interface Game {
+  id: number;
+  name: string;
+  description: string;
+  paramDescription: string;
+  cognitiveDomain: CognitiveDomain[];
+  gameParam: GameParam[];
+  params: Param[];
+};
+
+interface GameParam {
+  id: number;
+  maxValue: number;
+  minValue: number;
+  paramType2Content: [{
+    id: number;
+    name: string;
+  }];
+  param: Param;
+};
+
+interface Param {
+  id: number;
+  name: string;
+  className: string;
+  type: number;
+  unit: string;
+  contextualHelp: string;
+};
 
 @Injectable({
   providedIn: 'root'
@@ -14,8 +44,8 @@ export class GamesApiService {
   * Obtiene todos los juegos cargados.
   * @return una lista de juegos.
   */
-  getGames(): Observable<any> {
-    return this.http.get(`http://${environment.ip}:8080/${this.entity}`);
+  getGames(): Observable<Game[]> {
+    return this.http.get<Game[]>(`http://${environment.ip}:8080/${this.entity}`);
   }
 
   /**
@@ -23,8 +53,8 @@ export class GamesApiService {
   * @param id Id del juego.
   * @returns Observable del juego.
   */
-  getGameById(id: number): Observable<any> {
-    return this.http.get(`http://${environment.ip}:8080/${this.entity}/${id}`);
+  getGameById(id: number): Observable<Game> {
+    return this.http.get<Game>(`http://${environment.ip}:8080/${this.entity}/${id}`);
   }
   
   /**
@@ -32,7 +62,8 @@ export class GamesApiService {
    * @param cognitiveDomains lista de dominios cognitivos
    * @returns Juegos que poseen alguno de los dominios cognitivos seleccionados.
    */
-  getGamesFilteredByCD(cognitiveDomains: string []): Observable<any> {
-    return this.http.post(`http://${environment.ip}:8080/${this.entity}/cognitiveDomains`,cognitiveDomains);
+  getGamesFilteredByCD(cognitiveDomains: string[]): Observable<Game[]> {
+    const params = new HttpParams().set('cognitiveDomains', cognitiveDomains.join(','));
+    return this.http.get<Game[]>(`http://${environment.ip}:8080/${this.entity}/cognitiveDomains`, { params: params });
   }
 }
