caa0082f92714965c4263ba926c436fb7e9b36b4
ArmatorX <17768182+ArmatorX@users.noreply.github.com>
2022-08-17 18:49:02 -0300

    :art: Style (Tables): Reduce max display decimals to 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { RouterModule } from '@angular/router';
-import { IonicModule, IonicRouteStrategy, IonText } from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { FormsModule }    from '@angular/forms';
-
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es-AR';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
 import { HttpClientModule } from '@angular/common/http';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ResultsModule } from './results/results.module';
 import { SharedModule } from './shared/shared.module';
 import { PatientsModule } from './patients/patients.module';
 
+registerLocaleData(localeEs, 'es-AR');
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -31,7 +31,10 @@ import { PatientsModule } from './patients/patients.module';
     RouterModule.forRoot([])
   ],              
   exports: [RouterModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: LOCALE_ID, useValue: 'es-AR' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
